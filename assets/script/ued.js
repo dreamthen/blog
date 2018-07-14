bd212b0f3d93696aad6200e5bce86506d2ed7862
JameJJ <quattrohym@163.com>
2018-07-14 12:14:26 +0800

    Changes to be committed: 	new file:   assets/script/ued.js 	modified:   assets/style/style.css 	modified:   indexUed.html

---
+++ assets/script/ued.js
@@ -0,0 +1,9 @@
+(
+    function() {
+        var btn = document.getElementsByClassName("td-button");
+        var btnObj = btn[0];
+        btnObj.addEventListener("click", function(e) {
+            window.location.href="https://every-component.github.io/blog/";
+        })
+    }
+)(window)
